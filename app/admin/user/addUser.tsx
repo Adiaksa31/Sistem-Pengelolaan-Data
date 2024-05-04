7f4b57d158b9327772f3d914460657fb8adfffed
Adiaksa31 <programerbiasa@gmail.com>
2024-05-04 22:14:04 +0800

    fitur tambah data user

---
+++ app/admin/user/addUser.tsx
@@ -0,0 +1,150 @@
+'use client'
+import { SyntheticEvent, useState } from "react";
+import BtnData from "../components/btnData";
+import { useRouter } from "next/navigation";
+
+const token2 = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VyIjp7ImlkIjoxLCJuYW1hIjoiQWd1bmciLCJlbWFpbCI6ImVtYWlsQGdtYWlsLmNvbSIsIm5vbW9yIjoiMTExMTExMSIsInBvc2lzaV9pZCI6MSwiY2FiYW5nX2lkIjoxLCJzdGF0dXNfdXNlciI6InllcyIsImNyZWF0ZWRfYXQiOiIyMDI0LTA1LTAyVDExOjA3OjU1LjAwMFoiLCJ1cGRhdGVkX2F0IjoiMjAyNC0wNS0wMlQxMTowNzo1NS4wMDBaIn0sImlhdCI6MTcxNDgxNDYzNiwiZXhwIjoxNzE0OTAxMDM2fQ.5Tiz9uqhFNPFdMZ9gSf9gvXuCvrf8-ioaJjafHJ0yU4';
+
+export default function AddUser() {
+  const [nama, setNama] = useState("");
+  const [email, setEmail] = useState("");
+  const [nomor, setNomor] = useState("");
+  const [password, setPassword] = useState("");
+  const [posisi_id, setPosisi_id] = useState("");
+  const [cabang_id, setCabang_id] = useState("");
+
+  const router = useRouter();
+
+  async function addUser(e: SyntheticEvent) {
+    e.preventDefault();
+
+    try {
+      const preparedData = {
+        nama,
+        email,
+        nomor,
+        password,
+        posisi_id,
+        cabang_id,
+      };
+
+      const response = await fetch('http://localhost:3000/api/user/store', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+          'Authorization': `Bearer ${token2}`, 
+        },
+        body: JSON.stringify(preparedData),
+      });
+
+      if (response.ok) {
+        console.log('Data berhasil ditambahkan');
+        router.push('/success'); 
+      } else {
+        throw new Error('Gagal menambahkan data');
+      }
+    } catch (error) {
+        console.error('Error:', error as Error); 
+    }
+  }
+    const modalContent = (
+        <div className="p-4">
+          <h1 className="text-center font-bold text-xl">Tambah Data user</h1>
+          <br />
+          <form onSubmit={addUser} className="w-full max-w-lg">
+                <div className="flex flex-wrap -mx-3 mb-6">
+                <div className="w-full px-3">
+                    <label className="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2">
+                      Nama
+                    </label>
+                    <input className="appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white focus:border-gray-500" id="grid-first-name" type="text" placeholder="Masukkan Nama..." 
+                    value={nama}
+                    onChange={e=>setNama(e.target.value)}
+                    />
+                  </div>
+                </div>
+                <div className="flex flex-wrap -mx-3 mb-6">
+                <div className="w-full px-3">
+                    <label className="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2">
+                      Email
+                    </label>
+                    <input className="appearance-none block w-full bg-gray-200 text-gray-700 border border-red-500 rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white focus:border-gray-500" id="grid-first-name" type="text" placeholder="Masukkan Email..." 
+                     value={email}
+                     onChange={e=>setEmail(e.target.value)}
+                     />
+                    <p className="text-red-500 text-xs italic">Username sudah digunakan.</p>
+                  </div>
+                </div>
+                <div className="flex flex-wrap -mx-3 mb-6">
+                <div className="w-full px-3">
+                    <label className="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2">
+                      Nomor
+                    </label>
+                    <input className="appearance-none block w-full bg-gray-200 text-gray-700 border border-red-500 rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white focus:border-gray-500" id="grid-first-name" type="text" placeholder="Masukkan Nomor..." 
+                     value={nomor}
+                     onChange={e=>setNomor(e.target.value)}
+                     />
+                    <p className="text-red-500 text-xs italic">Username sudah digunakan.</p>
+                  </div>
+                </div>
+                <div className="flex flex-wrap -mx-3 mb-6">
+                  <div className="w-full px-3">
+                    <label className="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2" >
+                      Password
+                    </label>
+                    <input className="appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white focus:border-gray-500" id="grid-password" type="password" placeholder="******************" 
+                     value={password}
+                     onChange={e=>setPassword(e.target.value)}
+                    />
+                    <p className="text-gray-600 text-xs italic">Buat Password yang unik dan mudah diingat.</p>
+                  </div>
+                </div>
+                <div className="flex flex-wrap -mx-3 mb-2">
+                <div className="w-full md:w-1/2 px-3 mb-6 md:mb-0">
+                    <label className="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2">
+                      Posisi
+                    </label>
+                    <div className="relative">
+                      <input className="block appearance-none w-full bg-gray-200 border border-gray-200 text-gray-700 py-3 px-4 pr-8 rounded leading-tight focus:outline-none focus:bg-white focus:border-gray-500" id="grid-state"
+                       value={posisi_id}
+                       onChange={e=>setPosisi_id(e.target.value)}
+                       />
+                      {/* <option disabled selected>-- Pilih Posisi --</option>
+                        <option>1</option>
+                        <option>2</option> */}
+                    
+                      <div className="pointer-events-none absolute inset-y-0 right-0 flex items-center px-2 text-gray-700">
+                        <svg className="fill-current h-4 w-4" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20"><path d="M9.293 12.95l.707.707L15.657 8l-1.414-1.414L10 10.828 5.757 6.586 4.343 8z"/></svg>
+                      </div>
+                    </div>
+                  </div>
+                  <div className="w-full md:w-1/2 px-3 mb-6 md:mb-0">
+                    <label className="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2">
+                      Cabang
+                    </label>
+                    <div className="relative">
+                      <input className="block appearance-none w-full bg-gray-200 border border-gray-200 text-gray-700 py-3 px-4 pr-8 rounded leading-tight focus:outline-none focus:bg-white focus:border-gray-500" id="grid-state"
+                       value={cabang_id}
+                       onChange={e=>setCabang_id(e.target.value)} />
+                      {/* <option disabled selected>-- Pilih Cabang --</option>
+                        <option>1</option>
+                        <option>2</option> */}
+                      
+                      <div className="pointer-events-none absolute inset-y-0 right-0 flex items-center px-2 text-gray-700">
+                        <svg className="fill-current h-4 w-4" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20"><path d="M9.293 12.95l.707.707L15.657 8l-1.414-1.414L10 10.828 5.757 6.586 4.343 8z"/></svg>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+                <button className="px-4 mt-5 py-1 bg-green-600 hover:bg-green-700 text-white rounded focus:outline-none" onClick={addUser}>Submit</button>
+          </form>
+        </div>
+      );
+    return (
+        <>
+        <BtnData
+           content={modalContent} 
+          ></BtnData>
+        </>
+    )
+}
