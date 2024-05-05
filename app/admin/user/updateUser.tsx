7d73806efdbb785712883a6a68cdb6611d271faf
Gede Yuda Aditya <gdyudaaditya123@gmail.com>
2024-05-05 22:54:08 +0800

    Update edit data, mengambil id

--- app/admin/user/updateUser.tsx
+++ app/admin/user/updateUser.tsx
@@ -3,31 +3,42 @@
 import { SyntheticEvent, useState } from "react";
 import BtnEditData from "../components/btnEditData";
 
-const token4 = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VyIjp7ImlkIjoxLCJuYW1hIjoiQWd1bmciLCJlbWFpbCI6ImVtYWlsQGdtYWlsLmNvbSIsIm5vbW9yIjoiMTExMTExMSIsInBvc2lzaV9pZCI6MSwiY2FiYW5nX2lkIjoxLCJzdGF0dXNfdXNlciI6InllcyIsImNyZWF0ZWRfYXQiOiIyMDI0LTA1LTAyVDExOjA3OjU1LjAwMFoiLCJ1cGRhdGVkX2F0IjoiMjAyNC0wNS0wMlQxMTowNzo1NS4wMDBaIn0sImlhdCI6MTcxNDgxNDYzNiwiZXhwIjoxNzE0OTAxMDM2fQ.5Tiz9uqhFNPFdMZ9gSf9gvXuCvrf8-ioaJjafHJ0yU4';
+const token4 = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VyIjp7ImlkIjoxLCJuYW1hIjoiQWd1bmciLCJlbWFpbCI6ImVtYWlsQGdtYWlsLmNvbSIsIm5vbW9yIjoiMTExMTExMSIsInBvc2lzaV9pZCI6MSwiY2FiYW5nX2lkIjoxLCJzdGF0dXNfdXNlciI6InllcyIsImNyZWF0ZWRfYXQiOiIyMDI0LTA1LTAyVDExOjA3OjU1LjAwMFoiLCJ1cGRhdGVkX2F0IjoiMjAyNC0wNS0wMlQxMTowNzo1NS4wMDBaIn0sImlhdCI6MTcxNDkxOTc1NiwiZXhwIjoxNzE1MDA2MTU2fQ.qPO3TJcVC8Xuk4fmRuVnHB4c5ewMclpcWiypAUeSJlc';
 
 type User = {
     id: number;
     nama: string;
     email: string;
     nomor: number;
+    posisi: any;
+    cabang: any;
     password: any;
+    status: string;
   }
 
   export default function UpdateUser({ user }: { user: User }) {
+    const [id, setId] = useState(user.id);
     const [nama, setNama] = useState(user.nama);
     const [email, setEmail] = useState(user.email);
     const [nomor, setNomor] = useState(user.nomor);
     const [password, setPassword] = useState(user.password);
+    const [posisi_id, setPosisi_id] = useState(user.posisi.id);
+    const [cabang_id, setCabang_id] = useState(user.cabang.id);
+    const [status, setStatus] = useState(user.status);
     const [error, setError] = useState<string | null>(null); 
   
     async function handelUpdateUser(e: SyntheticEvent) {
       e.preventDefault();
       try {
         const preparedData = {
+          id,
           nama,
           email,
           nomor,
+          posisi_id,
+          cabang_id,
           password,
+          status,
         };
 
         const response = await fetch('http://localhost:3000/api/user/update', {
@@ -61,6 +72,8 @@ type User = {
           <h1 className="text-center font-bold text-xl">Edit Data {user.nama}</h1>
           <br />
           <form onSubmit={handelUpdateUser} className="w-full max-w-lg">
+                {/* hidden id */}
+                <input type="hidden" name="id" value={id} />
                 <div className="flex flex-wrap -mx-3 mb-6">
                 <div className="w-full px-3">
                     <label className="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2">
@@ -108,7 +121,60 @@ type User = {
                     <p className="text-gray-600 text-xs italic">Buat Password yang unik dan mudah diingat.</p>
                   </div>
                 </div>
-             
+                <div className="flex flex-wrap -mx-3 mb-2">
+                <div className="w-full md:w-1/3 px-3 mb-6 md:mb-0">
+                    <label className="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2">
+                      Posisi
+                    </label>
+                    <div className="relative">
+                      <input className="block appearance-none w-full bg-gray-200 border border-gray-200 text-gray-700 py-3 px-4 pr-8 rounded leading-tight focus:outline-none focus:bg-white focus:border-gray-500" id="grid-state"
+                       value={posisi_id}
+                       onChange={e=>setPosisi_id(e.target.value)}
+                       />
+                      {/* <option disabled selected>-- Pilih Posisi --</option>
+                        <option>1</option>
+                        <option>2</option> */}
+                    
+                      <div className="pointer-events-none absolute inset-y-0 right-0 flex items-center px-2 text-gray-700">
+                        <svg className="fill-current h-4 w-4" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20"><path d="M9.293 12.95l.707.707L15.657 8l-1.414-1.414L10 10.828 5.757 6.586 4.343 8z"/></svg>
+                      </div>
+                    </div>
+                  </div>
+                  <div className="w-full md:w-1/3 px-3 mb-6 md:mb-0">
+                    <label className="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2">
+                      Cabang
+                    </label>
+                    <div className="relative">
+                      <input className="block appearance-none w-full bg-gray-200 border border-gray-200 text-gray-700 py-3 px-4 pr-8 rounded leading-tight focus:outline-none focus:bg-white focus:border-gray-500" id="grid-state"
+                       value={cabang_id}
+                       onChange={e=>setCabang_id(e.target.value)} />
+                      {/* <option disabled selected>-- Pilih Cabang --</option>
+                        <option>1</option>
+                        <option>2</option> */}
+                      
+                      <div className="pointer-events-none absolute inset-y-0 right-0 flex items-center px-2 text-gray-700">
+                        <svg className="fill-current h-4 w-4" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20"><path d="M9.293 12.95l.707.707L15.657 8l-1.414-1.414L10 10.828 5.757 6.586 4.343 8z"/></svg>
+                      </div>
+                    </div>
+                  </div>
+                  <div className="w-full md:w-1/3 px-3 mb-6 md:mb-0">
+                    <label className="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2">
+                      Status
+                    </label>
+                    <div className="relative">
+                      <input className="block appearance-none w-full bg-gray-200 border border-gray-200 text-gray-700 py-3 px-4 pr-8 rounded leading-tight focus:outline-none focus:bg-white focus:border-gray-500" id="grid-state"
+                       value={status}
+                       onChange={e=>setStatus(e.target.value)} />
+                      {/* <option disabled selected>-- Pilih Status --</option>
+                        <option>1</option>
+                        <option>2</option> */}
+                      
+                      <div className="pointer-events-none absolute inset-y-0 right-0 flex items-center px-2 text-gray-700">
+                        <svg className="fill-current h-4 w-4" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20"><path d="M9.293 12.95l.707.707L15.657 8l-1.414-1.414L10 10.828 5.757 6.586 4.343 8z"/></svg>
+                      </div>
+                    </div>
+                    </div>
+                </div>
           </form>
         </div>
       );
