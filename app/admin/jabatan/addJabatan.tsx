0a009bad1c87998fbe1aa84fd673c0e317e570b9
Adiaksa31 <programerbiasa@gmail.com>
2024-05-06 14:22:14 +0800

    add delete update jabatan

---
+++ app/admin/jabatan/addJabatan.tsx
@@ -0,0 +1,67 @@
+'use client'
+import { SyntheticEvent, useState } from "react";
+import BtnData from "../components/btnData";
+
+const token2 = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VyIjp7ImlkIjoxLCJuYW1hIjoiQWd1bmciLCJlbWFpbCI6ImVtYWlsQGdtYWlsLmNvbSIsIm5vbW9yIjoiMTExMTExMSIsInBvc2lzaV9pZCI6MSwiY2FiYW5nX2lkIjoxLCJzdGF0dXNfdXNlciI6InllcyIsImNyZWF0ZWRfYXQiOiIyMDI0LTA1LTAyVDExOjA3OjU1LjAwMFoiLCJ1cGRhdGVkX2F0IjoiMjAyNC0wNS0wMlQxMTowNzo1NS4wMDBaIn0sImlhdCI6MTcxNDk2NTYzMSwiZXhwIjoxNzE1MDUyMDMxfQ.pAWcRHpfq4UREZVwAKSOi-OspGGG-bt3WO7PJLxdcQ8';
+
+export default function AddJabatan() {
+  const [nama, setNama] = useState("");
+
+  async function addJabatan(e: SyntheticEvent) {
+    e.preventDefault();
+  
+    try {
+      const preparedData = {
+        nama,
+      };
+  
+      const response = await fetch('http://localhost:3000/api/jabatan/store', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+          'Authorization': `Bearer ${token2}`,
+        },
+        body: JSON.stringify(preparedData),
+      });
+  
+      if (!response.ok) {
+  
+        const errorData = await response.json();
+        throw new Error(`Gagal menambahkan data: ${errorData.message || 'Unknown error'}`);
+      }
+  
+      console.log('Data berhasil ditambahkan');
+      window.location.reload();
+      return response;
+    } catch (error) {
+      console.error('Error:', error as Error);
+    }
+  }  
+ 
+    const modalContent = (
+        <div className="p-4">
+          <h1 className="text-center font-bold text-xl">Tambah Data Jabatan</h1>
+          <br />
+          <form onSubmit={addJabatan} className="w-full max-w-lg">
+                <div className="flex flex-wrap -mx-3 mb-6">
+                <div className="w-full px-3">
+                    <label className="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2">
+                      Nama Jabatan
+                    </label>
+                    <input className="appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white focus:border-gray-500" id="grid-first-name" type="text" placeholder="Masukkan Nama..." 
+                    value={nama}
+                    onChange={e=>setNama(e.target.value)}
+                    />
+                  </div>
+                </div>
+          </form>
+        </div>
+      );
+    return (
+        <>
+        <BtnData
+           content={modalContent} formSubmit={addJabatan}
+          ></BtnData>
+        </>
+    )
+}
--- app/admin/jabatan/page.tsx
+++ app/admin/jabatan/page.tsx
@@ -1,7 +1,9 @@
 "use client";
 import NavAdmAts from "../components/navAdmAts";
 import NavAdmBwh from "../components/navAdmBwh";
-import BtnData from "../components/btnData";
+import AddJabatan from "./addJabatan";
+import DeleteJabatan from "./deleteJabatan";
+import UpdateJabatan from "./updateJabatan";
 import Table from "../components/table";
 import Aksi from "../components/aksi";
 import Pagination from "../components/pagination";
@@ -29,6 +31,7 @@ async function getPosisis() {
 }
 type Posisi = {
   id: number;
+  nama: string;
   nama_posisi: string;
   status: string;
 }
@@ -50,72 +53,7 @@ export default function Jabatan() {
 
     fetchData();
   }, []);
-
-  
-  const modalContent = (
-    <div className="p-4">
-      <h1 className="text-center font-bold text-xl">Tambah Data jabatan</h1>
-      <br />
-      <form className="w-full max-w-lg">
-            <div className="flex flex-wrap -mx-3 mb-6">
-            <div className="w-full px-3">
-                <label className="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2">
-                  Nama jabatan
-                </label>
-                <input className="appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white focus:border-gray-500" id="grid-first-name" type="text" placeholder="Masukkan Nama..." />
-              </div>
-            </div>
-            <div className="flex flex-wrap -mx-3 mb-2">
-            <div className="w-full px-3 mb-6 md:mb-0">
-                <label className="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2">
-                  Status
-                </label>
-                <div className="relative">
-                  <select className="block appearance-none w-full bg-gray-200 border border-gray-200 text-gray-700 py-3 px-4 pr-8 rounded leading-tight focus:outline-none focus:bg-white focus:border-gray-500" id="grid-state">
-                  <option disabled selected>-- Pilih Status --</option>
-                    <option>Yes</option>
-                  </select>
-                  <div className="pointer-events-none absolute inset-y-0 right-0 flex items-center px-2 text-gray-700">
-                    <svg className="fill-current h-4 w-4" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20"><path d="M9.293 12.95l.707.707L15.657 8l-1.414-1.414L10 10.828 5.757 6.586 4.343 8z"/></svg>
-                  </div>
-                </div>
-              </div>
-            </div>
-      </form>
-    </div>
-  );
-  const mdlEditDataContent = (
-    <div className="p-4">
-      <h1 className="text-center font-bold text-xl">Edit Data Jabatan</h1>
-      <br />
-      <form className="w-full max-w-lg">
-            <div className="flex flex-wrap -mx-3 mb-6">
-            <div className="w-full px-3">
-                <label className="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2">
-                  Nama Jabatan
-                </label>
-                <input className="appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white focus:border-gray-500" id="grid-first-name" type="text" placeholder="Masukkan Nama..." />
-              </div>
-            </div>
-            <div className="flex flex-wrap -mx-3 mb-2">
-            <div className="w-full px-3 mb-6 md:mb-0">
-                <label className="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2">
-                  Status
-                </label>
-                <div className="relative">
-                  <select className="block appearance-none w-full bg-gray-200 border border-gray-200 text-gray-700 py-3 px-4 pr-8 rounded leading-tight focus:outline-none focus:bg-white focus:border-gray-500" id="grid-state">
-                  <option disabled selected>-- Pilih Status --</option>
-                    <option>Yes</option>
-                  </select>
-                  <div className="pointer-events-none absolute inset-y-0 right-0 flex items-center px-2 text-gray-700">
-                    <svg className="fill-current h-4 w-4" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20"><path d="M9.293 12.95l.707.707L15.657 8l-1.414-1.414L10 10.828 5.757 6.586 4.343 8z"/></svg>
-                  </div>
-                </div>
-              </div>
-            </div>
-      </form>
-    </div>
-  );
+ 
   const tableData = {
     headers: ['No', 'Nama Jabatan', 'Status', 'Action'],
     rows: posisiType.map((posisi, index) => [
@@ -124,7 +62,10 @@ export default function Jabatan() {
       posisi.status,
     
       <div key={`aksi-${index}`} className="container mx-auto">
-         <Aksi content={mdlEditDataContent}/></div>
+         <Aksi>
+         <UpdateJabatan posisi={posisi} />
+        <DeleteJabatan {...posisi} /> </Aksi>
+        </div>
     ]),
   };
  
@@ -148,9 +89,7 @@ export default function Jabatan() {
                   </svg>
               </div>
           </div>
-          <BtnData
-           content={modalContent}
-          ></BtnData>
+          <AddJabatan></AddJabatan>
         </div>
       </div>
     <div>
