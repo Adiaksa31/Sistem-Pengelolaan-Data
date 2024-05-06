fa7412fee4ac3e9f93304a4ba5d156ee2c78ff89
Adiaksa31 <programerbiasa@gmail.com>
2024-05-06 16:55:00 +0800

    crud pekerjaan fix

--- app/admin/pekerjaan/page.tsx
+++ app/admin/pekerjaan/page.tsx
@@ -1,85 +1,67 @@
-
+'use client'
 import NavAdmAts from "../components/navAdmAts";
 import NavAdmBwh from "../components/navAdmBwh";
-import BtnData from "../components/btnData";
+import AddPekerjaan from "./addPekerjaan";
+import UpdatePekerjaan from "./updatePekerjaan";
+import DeletePekerjaan from "./deletePekerjaan";
 import Table from "../components/table";
 import Aksi from "../components/aksi";
 import Pagination from "../components/pagination";
+import { useState, useEffect } from 'react';
+
+// Ini masih cara manual ngeset token, nanti bakal diubah pake cara otomatis (biasanya kamu ambil dari session aplikasi)
+const token = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VyIjp7ImlkIjoxLCJuYW1hIjoiQWd1bmciLCJlbWFpbCI6ImVtYWlsQGdtYWlsLmNvbSIsIm5vbW9yIjoiMTExMTExMSIsInBvc2lzaV9pZCI6MSwiY2FiYW5nX2lkIjoxLCJzdGF0dXNfdXNlciI6InllcyIsImNyZWF0ZWRfYXQiOiIyMDI0LTA1LTAyVDExOjA3OjU1LjAwMFoiLCJ1cGRhdGVkX2F0IjoiMjAyNC0wNS0wMlQxMTowNzo1NS4wMDBaIn0sImlhdCI6MTcxNDk2NTYzMSwiZXhwIjoxNzE1MDUyMDMxfQ.pAWcRHpfq4UREZVwAKSOi-OspGGG-bt3WO7PJLxdcQ8';
+
+async function getPekerjaans() {
+  const res = await fetch('http://localhost:3000/api/pekerjaan/get',{
+    method: 'POST',
+    headers:{
+      'Authorization': 'Bearer ' + token,
+    }}).then(response => response.json())
+		.then(response => {
+      if (response.status === 'error') {
+      } else {
+        return response.data;
+      }
+    })
+		.catch(err => console.error(err));
+
+  return res;
+}
+type Pekerjaan = {
+  id: number;
+  nama: string;
+  nama_pekerjaan: string;
+}
+
 
 export default function Pekerjaan() {
-  const modalContent = (
-    <div className="p-4">
-      <h1 className="text-center font-bold">Tambah Data Pekerjaan</h1>
-      <br />
-      <form className="w-full max-w-lg">
-            <div className="flex flex-wrap -mx-3 mb-6">
-              <div className="w-full px-3">
-                <label className="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2">
-                  Nama Pekerjaan
-                </label>
-                <input className="appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white focus:border-gray-500" id="grid-first-name" type="text" placeholder="Masukkan Nama Pekerjaan..." />
-              </div>
-            </div>
-            <div className="flex flex-wrap -mx-3 mb-2">
-            <div className="w-full px-3 mb-6 md:mb-0">
-                <label className="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2">
-                  Status
-                </label>
-                <div className="relative">
-                  <select className="block appearance-none w-full bg-gray-200 border border-gray-200 text-gray-700 py-3 px-4 pr-8 rounded leading-tight focus:outline-none focus:bg-white focus:border-gray-500" id="grid-state">
-                  <option disabled selected>-- Pilih Status --</option>
-                    <option>Yes</option>
-                    <option>No</option>
-                  </select>
-                  <div className="pointer-events-none absolute inset-y-0 right-0 flex items-center px-2 text-gray-700">
-                    <svg className="fill-current h-4 w-4" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20"><path d="M9.293 12.95l.707.707L15.657 8l-1.414-1.414L10 10.828 5.757 6.586 4.343 8z"/></svg>
-                  </div>
-                </div>
-              </div>
-            </div>
-      </form>
-    </div>
-  );
-  const mdlEditDataContent = (
-    <div className="p-4">
-      <h1 className="text-center font-bold">Edit Data Pekerjaan</h1>
-      <br />
-      <form className="w-full max-w-lg">
-            <div className="flex flex-wrap -mx-3 mb-6">
-              <div className="w-full px-3">
-                <label className="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2">
-                  Nama Pekerjaan
-                </label>
-                <input className="appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white focus:border-gray-500" id="grid-first-name" type="text" placeholder="Masukkan Nama Pekerjaan..." />
-              </div>
-            </div>
-            <div className="flex flex-wrap -mx-3 mb-2">
-            <div className="w-full px-3 mb-6 md:mb-0">
-                <label className="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2">
-                  Status
-                </label>
-                <div className="relative">
-                  <select className="block appearance-none w-full bg-gray-200 border border-gray-200 text-gray-700 py-3 px-4 pr-8 rounded leading-tight focus:outline-none focus:bg-white focus:border-gray-500" id="grid-state">
-                  <option disabled selected>-- Pilih Status --</option>
-                    <option>Yes</option>
-                    <option>No</option>
-                  </select>
-                  <div className="pointer-events-none absolute inset-y-0 right-0 flex items-center px-2 text-gray-700">
-                    <svg className="fill-current h-4 w-4" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20"><path d="M9.293 12.95l.707.707L15.657 8l-1.414-1.414L10 10.828 5.757 6.586 4.343 8z"/></svg>
-                  </div>
-                </div>
-              </div>
-            </div>
-      </form>
-    </div>
-  );
+  const [pekerjaans, setPekerjaans] = useState([]);
+  const pekerjaanType = pekerjaans as Pekerjaan[];
+
+  useEffect(() => {
+    async function fetchData() {
+      try {
+        const pekerjaanData = await getPekerjaans();
+        setPekerjaans(pekerjaanData);
+      } catch (error) {
+        console.error('Error fetching data:', error);
+      }
+    }
+
+    fetchData();
+  }, []);
 
   const tableData = {
-    headers: ['No', 'Nama Pekerjaan', 'Status', 'Action'],
-    rows: [
-      [1, 'Karyawan Swasta', 'Yes', <div key="Aksi" className="container mx-auto"><Aksi content={mdlEditDataContent}/></div>],
-      
-    ],
+    headers: ['No', 'Nama Pekerjaan', 'Action'],
+    rows: pekerjaanType.map((pekerjaan, index) => [
+      index + 1,
+      pekerjaan.nama_pekerjaan,
+    
+      <div key={`aksi-${index}`} className="container mx-auto">
+        <Aksi> <UpdatePekerjaan pekerjaan={pekerjaan} /><DeletePekerjaan {...pekerjaan} /></Aksi>
+      </div>
+    ]),
   };
   return (
   <>
@@ -102,9 +84,7 @@ export default function Pekerjaan() {
                   </svg>
               </div>
           </div>
-          <BtnData
-           content={modalContent}
-          ></BtnData>
+          <AddPekerjaan></AddPekerjaan>
         </div>
       </div>
       <div>
