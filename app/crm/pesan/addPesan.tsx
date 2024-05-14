e79282396099bafa536c6c90bd4fbd1df83a314c
Adiaksa31 <programerbiasa@gmail.com>
2024-05-14 17:49:13 +0800

    tambah data pesan select nomor wa

--- app/crm/pesan/addPesan.tsx
+++ app/crm/pesan/addPesan.tsx
@@ -1,7 +1,8 @@
 'use client'
-import { SyntheticEvent, useState, useEffect } from "react";
+import { SyntheticEvent, useState, useEffect, ChangeEvent } from "react";
 import BtnData from "../components/btnData";
 import token from "../components/token";
+import Link from 'next/link';
 
 export default function AddPesan() {
   const [kategori_id, setKategori_id] = useState("");
@@ -22,6 +23,8 @@ export default function AddPesan() {
   const [crm_id, setCrm_id] = useState("");
   const [tujuan_user, setTujuan_user] = useState("");
   const [status_kontak, setStatus_kontak] = useState("pending");
+  const [query, setQuery] = useState('');
+  const [selected, setSelected] = useState<boolean>(false);
 
 
   async function addPesan(e: SyntheticEvent) {
@@ -72,7 +75,22 @@ export default function AddPesan() {
     }
   }  
 
-
+  async function getPelanggans() {
+    const res = await fetch('http://localhost:3000/api/pelanggan/get',{
+      method: 'POST',
+      headers:{
+        'Authorization': 'Bearer ' + token,
+      }}).then(response => response.json())
+      .then(response => {
+        if (response.status === 'error') {
+        } else {
+          return response.data;
+        }
+      })
+      .catch(err => console.error(err));
+  
+    return res;
+  }
   async function getKategoris() {
     const res = await fetch('http://localhost:3000/api/kategori/get',{
       method: 'POST',
@@ -105,6 +123,20 @@ export default function AddPesan() {
   
     return res;
   }
+  type Pelanggan = {
+    id: number;
+    nama: string;
+    email: string;
+    no_wa: number;
+    tgl_lahir: any;
+    agama: string;
+    id_pekerjaan: any;
+    pekerjaan: any;
+    jenis_kelamin: string;
+    kelurahan: string;
+    kecamatan: string;
+    kabupaten: string;
+  }  
   type Cabang = {
     id: number;
     nama_cabang: string;
@@ -117,14 +149,18 @@ export default function AddPesan() {
     nama_kategori: string;
     status: string;
   }
-
+  const [pelanggans, setPelanggans] = useState([]);
+  const pelangganType = pelanggans as Pelanggan[];
   const [cabangs, setCabangs] = useState([]);
   const cabangType = cabangs as Cabang[];
   const [kategoris, setKategoris] = useState([]);
   const kategoriType = kategoris as Kategori[];
+
   useEffect(() => {
     async function fetchData() {
       try {
+        const pelangganData = await getPelanggans();
+        setPelanggans(pelangganData);
         const cabangData = await getCabangs();
         setCabangs(cabangData);
         const kategoriData = await getKategoris();
@@ -136,22 +172,60 @@ export default function AddPesan() {
 
     fetchData();
   }, []);  
+  const filteredPelanggan = pelangganType.filter(pelanggan =>
+    pelanggan.no_wa.toString().includes(query)
+  );
+  
+  const handleSelect = (id: number, no_wa: number, nama: string) => {
+    setCustomer_id(id.toString());
+    setQuery(`${no_wa} - ${nama}`.toString());
+    setSelected(true);
+  };
+  const handleChange = (e: ChangeEvent<HTMLInputElement>) => {
+    setQuery(e.target.value);
+    setSelected(false); 
+  };
     const modalContent = (
         <div className="p-4">
           <h1 className="text-center font-bold text-xl">Tambah Data Pelanggan</h1>
           <br />
           <form onSubmit={addPesan} className="w-full max-w-lg">
           <div className="flex flex-wrap -mx-3 mb-6">
-                <div className="w-full px-3">
-                    <label className="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2">
-                      Nama Pelanggan
-                    </label>
-                    <input className="appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white focus:border-gray-500" id="grid-first-name" type="text" placeholder="Masukkan Nama..." 
-                    value={customer_id}
-                    onChange={e=>setCustomer_id(e.target.value)}
-                    />
-                  </div>
-                </div>
+      <div className="w-full px-3">
+        <label className="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2">
+          Nomor Wa Pelanggan
+        </label>
+        <input
+          type="text"
+          className="block appearance-none w-full bg-gray-200 border border-gray-200 text-gray-700 py-3 px-4 pr-8 rounded leading-tight focus:outline-none focus:bg-white focus:border-gray-500"
+          placeholder="Masukkan Nomor Wa Pelanggan"
+          value={query}
+          onChange={handleChange}
+         
+        />
+        {query && !selected && (
+          <div className="relative">
+            {filteredPelanggan.length > 0 ? (
+              <ul className="absolute z-10 bg-white border border-gray-300 w-full rounded">
+                {filteredPelanggan.map(pelanggan => (
+                  <li
+                    key={pelanggan.id}
+                    className="py-2 px-4 hover:bg-gray-100 cursor-pointer"
+                    onClick={() => handleSelect(pelanggan.id, pelanggan.no_wa, pelanggan.nama)}
+                  >
+                    {pelanggan.no_wa} - {pelanggan.nama}
+                  </li>
+                ))}
+              </ul>
+            ) : (
+             <span>Tidak ada data silakan <Link className="text-green-600" href="/crm/pelanggan">
+               Tambah Data
+           </Link></span>
+            )}
+          </div>
+        )}
+      </div>
+    </div>
                 <div className="flex flex-wrap -mx-3 mb-6">
                   <div className="w-full px-3">
                 <label className="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2">
@@ -264,23 +338,6 @@ export default function AddPesan() {
                   </div>
                 </div>
             </div>
-            </div>
-            <div className="flex flex-wrap -mx-3 mb-6">
-            <div className="w-full md:w-1/3 px-3 mb-6 md:mb-0">
-                    <label className="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2">
-                      Status
-                    </label>
-                    <div className="relative">
-                      <select className="block appearance-none w-full bg-gray-200 border border-gray-200 text-gray-700 py-3 px-4 pr-8 rounded leading-tight focus:outline-none focus:bg-white focus:border-gray-500" id="grid-state"
-                       value={status_kontak}
-                       onChange={e=>setStatus_kontak(e.target.value)} >
-                      <option disabled selected>-- Pending--</option>
-                      </select>
-                      <div className="pointer-events-none absolute inset-y-0 right-0 flex items-center px-2 text-gray-700">
-                        <svg className="fill-current h-4 w-4" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20"><path d="M9.293 12.95l.707.707L15.657 8l-1.414-1.414L10 10.828 5.757 6.586 4.343 8z"/></svg>
-                      </div>
-                    </div>
-                    </div>
             </div>
          </form>
         </div>
