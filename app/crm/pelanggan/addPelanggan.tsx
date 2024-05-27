563a3380c6606143a51885bc305127c38f718acb
Gede Yuda Aditya <gdyudaaditya123@gmail.com>
2024-05-27 18:39:48 +0800

    Update

--- app/crm/pelanggan/addPelanggan.tsx
+++ app/crm/pelanggan/addPelanggan.tsx
@@ -3,6 +3,7 @@ import { SyntheticEvent, useState, useEffect } from "react";
 import { useRouter } from "next/navigation";
 import BtnData from "../components/btnData";
 import token from "../components/token";
+const fetch2 = require('node-fetch');
 
 
 export default function AddPelanggan() {
@@ -77,24 +78,60 @@ export default function AddPelanggan() {
   }
 
   async function getKabupaten() {
-    const res = await fetch('https://emsifa.github.io/api-wilayah-indonesia/api/regencies/51.json',{
-      method: 'GET',
-      headers:{
-        'Authorization': 'Bearer ' + token,
-      }}).then(response => response.json())
-      .then(response => {
-        if (response.status === 'error') {
-        } else {
-          return response.data;
+    try {
+        const response = await fetch('/api/region/kabupaten', {
+            method: 'GET',
+            headers: {
+                'Authorization': 'Bearer ' + token,
+            }
+        });
+
+        if (!response.ok) {
+            throw new Error(`HTTP error! status: ${response.status}`);
         }
-      })
-      .catch(err => console.error(err));
-  
-    return res;
+
+        const provinces = await response.json();
+        console.log("Parsed JSON:", provinces);
+
+        return provinces;
+    } catch (error) {
+        console.error("Error fetching data:", error);
+        throw error;
+    }
   }
+
+  async function getKecamatan(id:string) {
+    try {
+      const response = await fetch('/api/region/kecamatan', {
+          method: 'GET',
+          headers: {
+              'Authorization': 'Bearer ' + token,
+          },
+          body: JSON.stringify({id:id})
+      });
+
+      if (!response.ok) {
+          throw new Error(`HTTP error! status: ${response.status}`);
+      }
+
+      const provinces = await response.json();
+      console.log("Parsed JSON:", provinces);
+
+      return provinces;
+    } catch (error) {
+        console.error("Error fetching data:", error);
+        throw error;
+    }
+  }
+
   
 
   type Kabupaten = {
+    id: number;
+    name: string;
+  }
+
+  type Kecamatan = {
     id: number;
     name: string;
   }
@@ -227,11 +264,13 @@ const kabupatenType = kabupatens as Kabupaten[];
                        value={kabupaten}
                        onChange={e=>setKabupaten(e.target.value)} >
                       <option selected value="" disabled >-- Pilih --</option>
-                      {kabupatenType.map(kabupaten => (
+                      {
+                      kabupatenType.map(kabupaten => (
                           <option key={kabupaten.id} value={kabupaten.id}>
                             {kabupaten.name}
                           </option>
-                        ))}
+                        ))
+                      }
                       </select>
                       <div className="pointer-events-none absolute inset-y-0 right-0 flex items-center px-2 text-gray-700">
                         <svg className="fill-current h-4 w-4" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20"><path d="M9.293 12.95l.707.707L15.657 8l-1.414-1.414L10 10.828 5.757 6.586 4.343 8z"/></svg>
@@ -248,9 +287,13 @@ const kabupatenType = kabupatens as Kabupaten[];
                        onChange={e=>setKecamatan(e.target.value)} >
                       <option selected value="" disabled >-- Pilih --</option>
                      
-                          <option>
-                            Tejakula
-                          </option>
+                          {
+                          kabupatenType.map(kabupaten => (
+                            <option key={kabupaten.id} value={kabupaten.id}>
+                              {kabupaten.name}
+                            </option>
+                          ))
+                          }
 
                       </select>
                       <div className="pointer-events-none absolute inset-y-0 right-0 flex items-center px-2 text-gray-700">
---
+++ pages/api/region/kecamatan.js
@@ -0,0 +1,22 @@
+// pages/api/kabupaten.js
+import fetch from "node-fetch";
+
+export default async function handler(req, res) {
+  try {
+    const response = await fetch(
+      "https://emsifa.github.io/api-wilayah-indonesia/api/districts/51" +
+        res.body.id +
+        ".json"
+    );
+    if (!response.ok) {
+      res.status(response.status).json({ error: "Failed to fetch data" });
+      return;
+    }
+
+    const data = await response.json();
+    res.status(200).json(data);
+  } catch (error) {
+    console.error("Error fetching data:", error);
+    res.status(500).json({ error: "Internal Server Error" });
+  }
+}
