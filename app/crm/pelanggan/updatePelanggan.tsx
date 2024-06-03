e59c14c7da5833fa395d7df2cd6deddde77b6f42
Adiaksa31 <programerbiasa@gmail.com>
2024-06-03 11:04:47 +0800

    belum fix

--- app/crm/pelanggan/updatePelanggan.tsx
+++ app/crm/pelanggan/updatePelanggan.tsx
@@ -33,6 +33,9 @@ type Pelanggan = {
     const [kelurahan, setKelurahan] = useState(pelanggan.kelurahan);
     const [kecamatan, setKecamatan] = useState(pelanggan.kecamatan);
     const [kabupaten, setKabupaten] = useState(pelanggan.kabupaten);
+    const [kelurahanName, setKelurahanName] = useState("");
+    const [kecamatanName, setKecamatanName] = useState("");
+    const [kabupatenName, setKabupatenName] = useState("");
     const [error, setError] = useState<string | null>(null); 
     const router = useRouter();
   
@@ -48,9 +51,9 @@ type Pelanggan = {
           agama,
           id_pekerjaan,
           jenis_kelamin,
-          kelurahan,
-          kecamatan,
-          kabupaten,
+          kelurahan: kelurahanName,
+          kecamatan: kecamatanName,
+          kabupaten: kabupatenName,
         };
 
         const response = await fetch('http://localhost:3000/api/pelanggan/update', {
@@ -95,11 +98,95 @@ type Pelanggan = {
     
       return res;
     }
+
+    
+  async function getKabupaten() {
+    try {
+        const response = await fetch('/api/region/kabupaten', {
+            method: 'GET',
+            headers: {
+                'Authorization': 'Bearer ' + token,
+            }
+        });
+
+        if (!response.ok) {
+            throw new Error(`HTTP error! status: ${response.status}`);
+        }
+
+        const provinces = await response.json();
+        console.log("Parsed JSON:", provinces);
+
+        return provinces;
+    } catch (error) {
+        console.error("Error fetching data:", error);
+        throw error;
+    }
+  }
+
+  async function getKecamatan(id: string) {
+    try {
+      const response = await fetch(`/api/region/kecamatan?id=${id}`, {
+        method: 'GET',
+        headers: {
+          'Authorization': 'Bearer ' + token,
+        }
+      });
+  
+      if (!response.ok) {
+        throw new Error(`HTTP error! status: ${response.status}`);
+      }
+  
+      const provinces = await response.json();
+      console.log("Parsed JSON:", provinces);
+  
+      return provinces;
+    } catch (error) {
+      console.error("Error fetching data:", error);
+      throw error;
+    }
+  }
+ 
+  async function getKelurahan(id: string) {
+    try {
+      const response = await fetch(`/api/region/kelurahan?id=${id}`, {
+        method: 'GET',
+        headers: {
+          'Authorization': 'Bearer ' + token,
+        }
+      });
+  
+      if (!response.ok) {
+        throw new Error(`HTTP error! status: ${response.status}`);
+      }
+  
+      const provinces = await response.json();
+      console.log("Parsed JSON:", provinces);
+  
+      return provinces;
+    } catch (error) {
+      console.error("Error fetching data:", error);
+      throw error;
+    }
+  }
+
+  type Region = {
+    id: number;
+    name: string;
+  }
   
     type Pekerjaan = {
       id: number;
       nama_pekerjaan: string;
     }
+
+    const [kabupatens, setKabupatens] = useState([]);
+    const [kecamatans, setKecamatans] = useState([]);
+    const [kelurahans, setKelurahans] = useState([]);
+  
+    const kabupatenType = kabupatens as Region[];
+    const kecamatanType = kecamatans as Region[];
+    const kelurahanType = kelurahans as Region[];
+  
     const [pekerjaans, setPekerjaans] = useState([]);
     const pekerjaanType = pekerjaans as Pekerjaan[];
     useEffect(() => {
@@ -107,12 +194,58 @@ type Pelanggan = {
         try {
           const pekerjaanData = await getPekerjaans();
           setPekerjaans(pekerjaanData);
+          const kabupatenData = await getKabupaten();
+          setKabupatens(kabupatenData);
+  
         } catch (error) {
           console.error('Error fetching data:', error);
         }
       }
       fetchData();
     }, []);
+
+    
+  useEffect(() => {
+    async function fetchKecamatanData() {
+      if (kabupaten) {
+        try {
+          const kecamatanData = await getKecamatan(kabupaten);
+          setKecamatans(kecamatanData);
+          
+          const selectedKabupaten = kabupatenType.find(item => item.id.toString() === kabupaten);
+          setKabupatenName(selectedKabupaten ? selectedKabupaten.name : "");
+
+        } catch (error) {
+          console.error('Error fetching kecamatan data:', error);
+        }
+      }
+    }
+    fetchKecamatanData();
+  }, [kabupaten]);
+
+  useEffect(() => {
+    async function fetchKelurahanData() {
+      if (kecamatan) {
+        try {
+          const kelurahanData = await getKelurahan(kecamatan);
+          setKelurahans(kelurahanData);
+          
+          const selectedKecamatan = kecamatanType.find(item => item.id.toString() === kecamatan);
+          setKecamatanName(selectedKecamatan ? selectedKecamatan.name : "");
+
+        } catch (error) {
+          console.error('Error fetching kelurahan data:', error);
+        }
+      }
+    }
+    fetchKelurahanData();
+  }, [kecamatan]);
+
+  useEffect(() => {
+    const selectedKelurahan = kelurahanType.find(item => item.id.toString() === kelurahan);
+    setKelurahanName(selectedKelurahan ? selectedKelurahan.name : "");
+  }, [kelurahan]);
+
     const modalContent = (
         <div className="p-4">
           <h1 className="text-center font-bold text-xl">Edit Data {pelanggan.nama}</h1>
@@ -205,68 +338,78 @@ type Pelanggan = {
                  onChange={e=>setTgl_lahir(e.target.value)}/>
               </div>
             </div>
-            <div className="flex flex-wrap -mx-3 mb-2">
-                  <div className="w-full md:w-1/3 px-3 mb-6 md:mb-0">
+            <div className="flex flex-wrap -mx-3 mb-6">
+                  <div className="w-full px-3">
                     <label className="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2">
                       Kabupaten
                     </label>
                     <div className="relative">
-                      <select className="block appearance-none w-full bg-gray-200 border border-gray-200 text-gray-700 py-3 px-4 pr-8 rounded leading-tight focus:outline-none focus:bg-white focus:border-gray-500" id="grid-state"
-                       value={kabupaten}
-                       onChange={e=>setKabupaten(e.target.value)} >
-                      <option selected disabled >-- Pilih --</option>
-                        <option>Buleleng</option>
+                    <select
+                        className="block appearance-none w-full bg-gray-200 border border-gray-200 text-gray-700 py-3 px-4 pr-8 rounded leading-tight focus:outline-none focus:bg-white focus:border-gray-500"
+                        id="grid-state"
+                        value={kabupaten}
+                        onChange={e => setKabupaten(e.target.value)}
+                      >
+                        <option selected disabled>Pilih Kabupaten</option>
+                        {kabupatenType.map((region) => (
+                          <option key={region.id} value={region.id}>{region.name}</option>
+                        ))}
                       </select>
                       <div className="pointer-events-none absolute inset-y-0 right-0 flex items-center px-2 text-gray-700">
                         <svg className="fill-current h-4 w-4" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20"><path d="M9.293 12.95l.707.707L15.657 8l-1.414-1.414L10 10.828 5.757 6.586 4.343 8z"/></svg>
                       </div>
                     </div>
                   </div>
-                  <div className="w-full md:w-1/3 px-3 mb-6 md:mb-0">
+                  </div>
+                  <div className="flex flex-wrap -mx-3 mb-6">
+                  <div className="w-full px-3">
                     <label className="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2">
                       Kecamatan
                     </label>
                     <div className="relative">
-                      <select className="block appearance-none w-full bg-gray-200 border border-gray-200 text-gray-700 py-3 px-4 pr-8 rounded leading-tight focus:outline-none focus:bg-white focus:border-gray-500" id="grid-state"
-                       value={kecamatan}
-                       onChange={e=>setKecamatan(e.target.value)} >
-                      <option selected disabled >-- Pilih --</option>
-                     
-                          <option>
-                            Tejakula
-                          </option>
-
+                    <select
+                        className="block appearance-none w-full bg-gray-200 border border-gray-200 text-gray-700 py-3 px-4 pr-8 rounded leading-tight focus:outline-none focus:bg-white focus:border-gray-500"
+                        id="grid-state"
+                        value={kecamatan}
+                        onChange={e => setKecamatan(e.target.value)}
+                      >
+                        <option selected disabled >Pilih Kecamatan</option>
+                        {kecamatanType.map((region) => (
+                          <option key={region.id} value={region.id}>{region.name}</option>
+                        ))}
                       </select>
                       <div className="pointer-events-none absolute inset-y-0 right-0 flex items-center px-2 text-gray-700">
                         <svg className="fill-current h-4 w-4" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20"><path d="M9.293 12.95l.707.707L15.657 8l-1.414-1.414L10 10.828 5.757 6.586 4.343 8z"/></svg>
                       </div>
                     </div>
                   </div>
-                  <div className="w-full md:w-1/3 px-3 mb-6 md:mb-0">
+                  </div>
+                  <div className="flex flex-wrap -mx-3 mb-6">
+                  <div className="w-full px-3">
                     <label className="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2">
                       Kelurahan
                     </label>
                     <div className="relative">
-                      <select className="block appearance-none w-full bg-gray-200 border border-gray-200 text-gray-700 py-3 px-4 pr-8 rounded leading-tight focus:outline-none focus:bg-white focus:border-gray-500" id="grid-state"
-                       value={kelurahan}
-                       onChange={e=>setKelurahan(e.target.value)}
-                       >
-                      <option selected disabled >-- Pilih --</option>
-                      
-                          <option>
-                              Bondalem
-                          </option>
- 
+                    <select
+                        className="block appearance-none w-full bg-gray-200 border border-gray-200 text-gray-700 py-3 px-4 pr-8 rounded leading-tight focus:outline-none focus:bg-white focus:border-gray-500"
+                        id="grid-state"
+                        value={kelurahan}
+                        onChange={e => setKelurahan(e.target.value)}
+                      >
+                        <option selected disabled>Pilih Kelurahan</option>
+                        {kelurahanType.map((region) => (
+                          <option key={region.id} value={region.id}>{region.name}</option>
+                        ))}
                       </select>
                       <div className="pointer-events-none absolute inset-y-0 right-0 flex items-center px-2 text-gray-700">
                         <svg className="fill-current h-4 w-4" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20"><path d="M9.293 12.95l.707.707L15.657 8l-1.414-1.414L10 10.828 5.757 6.586 4.343 8z"/></svg>
                       </div>
                     </div>
                   </div>
-                </div>
+                  </div>
           
-                <div className="flex flex-wrap -mx-3 mb-2">
-                <div className="w-full px-3 mb-6 md:mb-0">
+                <div className="flex flex-wrap -mx-3 mb-6">
+                <div className="w-full px-3">
                     <label className="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2">
                       Pekerjaan
                     </label>
