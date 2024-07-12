561355bae477b553e04403f20d7113f1f056e856
Adiaksa31 <programerbiasa@gmail.com>
2024-07-12 21:40:50 +0800

    test localstorage 4

--- app/admin/components/token.ts
+++ app/admin/components/token.ts
@@ -1,37 +1,20 @@
 'use client'
-import { useEffect, useState } from 'react';
+const token = localStorage.getItem('token');
 
-const useToken = () => {
-  const [token, setToken] = useState<string | null>(null);
-
-  useEffect(() => {
-    if (typeof window !== 'undefined') {
-      const token = localStorage.getItem('token');
-
-      if (!token) {
-        window.location.href = '/';
-      } else {
-        try {
-          const tokenData = JSON.parse(atob(token.split('.')[1]));
-          const exp = tokenData.exp;
-          const now = new Date().getTime() / 1000;
-
-          if (exp < now) {
+// check if valid token
+if (!token) {
+  window.location.href = '/';
+} else {
+    // check if token expired
+    if (token) {
+        const tokenData = JSON.parse(atob(token.split('.')[1]));
+        const exp = tokenData.exp;
+        const now = new Date().getTime() / 1000;
+        if (exp < now) {
             localStorage.removeItem('token');
             window.location.href = '/';
-          } else {
-            setToken(token);
-          }
-        } catch (error) {
-          console.error('Invalid token:', error);
-          localStorage.removeItem('token');
-          window.location.href = '/';
         }
-      }
     }
-  }, []);
+}
 
-  return token;
-};
-
-export default useToken;
+export default token;
--- app/user/components/TokenComponent.tsx
+++ app/user/components/TokenComponent.tsx
@@ -7,27 +7,19 @@ const TokenComponent: React.FC = () => {
   const [token, setToken] = useState<string | null>(null);
 
   useEffect(() => {
-    if (typeof window !== 'undefined') {
-      const storedToken = localStorage.getItem('token');
+    const storedToken = localStorage.getItem('token');
 
-      if (!storedToken) {
+    if (!storedToken) {
+      window.location.href = '/';
+    } else {
+      const tokenData = JSON.parse(atob(storedToken.split('.')[1]));
+      const exp = tokenData.exp;
+      const now = new Date().getTime() / 1000;
+      if (exp < now) {
+        localStorage.removeItem('token');
         window.location.href = '/';
       } else {
-        try {
-          const tokenData = JSON.parse(atob(storedToken.split('.')[1]));
-          const exp = tokenData.exp;
-          const now = new Date().getTime() / 1000;
-          if (exp < now) {
-            localStorage.removeItem('token');
-            window.location.href = '/';
-          } else {
-            setToken(storedToken); // Set the token if it is valid
-          }
-        } catch (error) {
-          console.error('Invalid token:', error);
-          localStorage.removeItem('token');
-          window.location.href = '/';
-        }
+        setToken(storedToken); // Set the token if it is valid
       }
     }
   }, []);
@@ -35,11 +27,6 @@ const TokenComponent: React.FC = () => {
   return null;
 };
 
-export const getToken = () => {
-  if (typeof window !== 'undefined') {
-    return localStorage.getItem('token');
-  }
-  return null;
-};
+export const getToken = () => localStorage.getItem('token');
 
 export default TokenComponent;
